883de35ac5b86a5a129c1c5d9094a13d1f8e6122
mmunier44 <munier1@gmail.com>
2018-08-31 10:28:45 -0400

    Auth Events and Index.html Adjusted events to point to cards api Adjusted index buttons to get cards

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -5,34 +5,56 @@ const store = require('../store.js')
 const api = require('./api.js')
 const ui = require('./ui.js')
 
-const onGetBooks = (event) => {
+const onGetCards = (event) => {
   event.preventDefault()
-  api.getBooks()
-    .then(ui.getBooksSuccess)
+  api.getCards()
+    .then(ui.getCardsSuccess)
     .catch(ui.failure)
 }
 
-const onClearBooks = (event) => {
+const onClearCards = (event) => {
   event.preventDefault()
   ui.clearBooks()
 }
 
-const onDeleteBook = (event) => {
+const onDeleteCard = (event) => {
   event.preventDefault()
-  const bookId = $(event.target).closest('section').data('id')
-  if (confirm('Are you sure you want to delete this book?')) {
-    api.deleteBook(bookId)
-      .then(() => onGetBooks(event))
+  const cardId = $(event.target).closest('section').data('id')
+  if (confirm('Are you sure you want to delete this card?')) {
+    api.deleteCard(cardId)
+      .then(() => onGetCards(event))
       .catch(ui.failure)
   }
 }
 
 const addHandlers = () => {
-  $('#getBooksButton').on('click', onGetBooks)
-  $('#clearBooksButton').on('click', onClearBooks)
-  $('.content').on('click', 'button', onDeleteBook)
+  $('#getCardsButton').on('click', onGetCards)
+  $('#clearCardsButton').on('click', onClearCards)
+  $('.content').on('click', 'button', onDeleteCard)
 }
 
 module.exports = {
   addHandlers
 }
+
+// const onGetBooks = (event) => {
+//   event.preventDefault()
+//   api.getBooks()
+//     .then(ui.getBooksSuccess)
+//     .catch(ui.failure)
+// }
+//
+// const onClearBooks = (event) => {
+//   event.preventDefault()
+//   ui.clearBooks()
+// }
+// /
+// const onDeleteBook = (event) => {
+//   event.preventDefault()
+//   const bookId = $(event.target).closest('section').data('id')
+//   if (confirm('Are you sure you want to delete this book?')) {
+//     api.deleteBook(bookId)
+//       .then(() => onGetBooks(event))
+//       .catch(ui.failure)
+//   }
+// }
