3f4e56efb31a9bb40ec9a110481fef2c2bbf5ded
mmunier44 <munier1@gmail.com>
2018-08-31 13:23:05 -0400

    Add onEventClicks
    On Auth Events inputs and Messages updated
    CLick events added

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -1,9 +1,105 @@
 'use strict'
 
-const config = require('../config.js')
+// const config = require('../config.js')
 const store = require('../store.js')
 const api = require('./api.js')
 const ui = require('./ui.js')
+const getFormFields = require('../../../lib/get-form-fields.js')
+
+const onSignUp = function (event) {
+  event.preventDefault()
+  $('#message').text('Sign Up Successful')
+  // console.log('submitted the form')
+  // console.log('event.target:', event.target)
+  const data = getFormFields(event.target)
+  // console.log('data from the form:', data)
+
+  api.signUp(data)
+    .then(ui.signUpSuccess)
+    .catch(ui.signUpFail)
+}
+
+const onPasswordChange = function (event) {
+  $('#message').text('Password Changed')
+  event.preventDefault()
+  // console.log('password changed')
+  const data = getFormFields(event.target)
+
+  api.passwordChange(data)
+    .then(ui.passwordChangeSuccess)
+    .catch(ui.passwordChangeInFail)
+}
+
+const onSignOut = function (event) {
+  $('#message').text('Sign Out Successful')
+  event.preventDefault()
+  // console.log('Signed Out')
+  const data = getFormFields(event.target)
+
+  api.signOut(data)
+    .then(ui.signOutSuccess)
+    .catch(ui.signOutFail)
+}
+
+const onListCards = function (event) {
+  $('#message').text('Cards List Loaded')
+  event.preventDefault()
+  console.log('Cards Shown')
+  // const data = getFormFields(event.target)
+
+  api.listCards()
+    .then(ui.listCardsSuccess)
+    .catch(ui.listCardsFail)
+}
+
+const onShowCard = function (event) {
+  $('#message').text('Card Shown')
+  event.preventDefault()
+  // console.log('Card Shown')
+  // let id
+  // id = getFormFields(event.target).id
+  // const data = getFormFields(event.target)
+
+  api.showCard()
+    .then(ui.showCardSuccess)
+    .catch(ui.showCardFail)
+}
+
+const onUpdateMove = (event) => {
+  event.preventDefault()
+  console.log('event logged', event)
+  const data = getFormFields(event.target)
+  console.log('store.card.id', store.card.id)
+  console.log('store check', store)
+  console.log('data check', data)
+  console.log('data.card', data.card)
+  console.log(data)
+  api.updateMove(data)
+    .then(ui.updateMoveSuccess)
+    .catch(ui.updateMoveFail)
+}
+
+const onNewCard = function (data, event) {
+  $('table').removeClass('hide')
+  console.log('newcardworking?')
+  // console.log('check data', data)
+  $('#message').text('New Card!')
+  api.newCard(event)
+    .then(ui.newCardSuccess)
+    .catch(ui.newCardFail)
+}
+
+const onSignIn = function (event) {
+  $('#message').text('Sign In Successful')
+  event.preventDefault()
+  // console.log('signed in')
+  const data = getFormFields(event.target)
+  // console.log('sign in data', data)
+
+  api.signIn(data)
+    .then(ui.signInSuccess)
+    .catch(ui.signInFail)
+}
 
 const onGetCards = (event) => {
   event.preventDefault()
@@ -31,10 +127,27 @@ const addHandlers = () => {
   $('#getCardsButton').on('click', onGetCards)
   $('#clearCardsButton').on('click', onClearCards)
   $('.content').on('click', 'button', onDeleteCard)
+  $('#sign-up').on('submit', onSignUp)
+  $('#sign-in').on('submit', onSignIn)
+  $('#change-password').on('submit', onPasswordChange)
+  $('#sign-out').on('submit', onSignOut)
+  $('#list-games').on('submit', onListCards)
+  $('#create-game').on('submit', onNewCard)
+  $('#show-game').on('submit', onShowCard)
+  // $('.cell').on('click,onGameboard')
+  $('#new-game').on('click', onNewCard)
 }
 
 module.exports = {
-  addHandlers
+  addHandlers,
+  onSignUp,
+  onSignIn,
+  onNewCard,
+  onUpdateMove,
+  onShowCard,
+  onListCards,
+  onSignOut,
+  onPasswordChange
 }
 
 // const onGetBooks = (event) => {
